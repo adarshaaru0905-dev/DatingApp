b7a6d9d7533b5333003c24268557ff4f297ef2c5
adarshaaru0905-dev <adarsh2903smshetty@gmail.com>
2024-03-13 19:46:44 +0530

    Second Section

---
+++ Datingapp/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using Datingapp.Data;
+using Datingapp.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Datingapp.Controllers;
+[ApiController]
+[Route("api/[controller]")] //
+
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await _context.Users.ToListAsync();
+        return users;
+
+    }
+
+    [HttpGet("{id}")] // Api/users/2
+    public async Task<ActionResult<AppUser>> GetUsers(int id)
+    {
+
+        return await _context.Users.FindAsync(id);
+
+    }
+
+
+}
---
+++ Datingapp/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Datingapp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Datingapp.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+
+    }
+    public DbSet<AppUser> Users { get; set; }
+}
